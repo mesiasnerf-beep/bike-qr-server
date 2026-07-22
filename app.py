08ffa9003b784379cba9016eaedf685972fa5285
mesiasnerf-beep <mesiasnerf@gmail.com>
2026-07-22 20:53:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="bike_qr_db"
+)
+
+@app.route("/")
+def home():
+    return "Bike QR Attendance API Running!"
+
+@app.route("/riders", methods=["GET"])
+def get_riders():
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM riders")
+    data = cursor.fetchall()
+    cursor.close()
+    return jsonify(data)
+
+
+@app.route("/register", methods=["POST"])
+def register():
+
+    data = request.json
+
+    cursor = db.cursor()
+
+    sql = """
+    INSERT INTO riders
+    (fullName, gradeSection, lrn, address, bikeColor,
+    bikeType, contactNumber, emergencyName,
+    emergencyNumber, username, password)
+
+    VALUES
+    (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+
+    values = (
+        data["fullName"],
+        data["gradeSection"],
+        data["lrn"],
+        data["address"],
+        data["bikeColor"],
+        data["bikeType"],
+        data["contactNumber"],
+        data["emergencyName"],
+        data["emergencyNumber"],
+        data["username"],
+        data["password"]
+    )
+
+    cursor.execute(sql, values)
+    db.commit()
+
+    cursor.close()
+
+    return jsonify({
+        "message": "Rider Registered Successfully!"
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000,debug=True)
